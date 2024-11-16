7d4d298f037a661562d42ec06f5997d6541b5fc2
abacaxiii123 <0000109012465xsp@al.educacao.sp.gov.br>
2024-11-15 21:59:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    ' O que e Python',
+    ' O Python e uma linguagem de programacao',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'o que é função?',
+    'Uma função é um bloco de codigo que executa aluma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Fraça?',
+    'A capital da França e Paris'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
